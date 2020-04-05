f172817d4d9fdaf04ed9e4c30c345bf686c428ec
fniemeyer <franziska.niemeyer@uni-bielefeld.de>
2020-04-05 11:48:59 +0200

    Add refract() Method in Vec3 class

--- src/Vec3.java
+++ src/Vec3.java
@@ -32,16 +32,24 @@ public class Vec3 {
         return e[2];
     }
 
-    public void negate() {
+    public Vec3 negate() {
         e[0] = - this.e[0];
         e[1] = - this.e[1];
         e[2] = - this.e[2];
+        return new Vec3(e[0], e[1], e[2]);
     }
 
     public Vec3 plus(Vec3 vector) {
         float ix = this.e[0] + vector.x();
         float iy = this.e[1] + vector.y();
         float iz = this.e[2] + vector.z();
+        return new Vec3(ix, iy, iz);
+    }
+
+    public Vec3 plus(float num) {
+        float ix = this.e[0] + num;
+        float iy = this.e[1] + num;
+        float iz = this.e[2] + num;
         return new Vec3(ix, iy, iz);
     }
 
@@ -89,6 +97,13 @@ public class Vec3 {
 
     public static Vec3 reflect(Vec3 v, Vec3 n) {
         return v.minus(n.multiply(dot(v, n) * 2));
+    }
+
+    public Vec3 refract(Vec3 uv, Vec3 n, float etai_over_etat) {
+        float cos_theta = dot(uv.negate(), n);
+        Vec3 r_out_parallel = (uv.plus(cos_theta)).multiply(etai_over_etat);
+        Vec3 r_out_perp = n.multiply(- (float) Math.sqrt(1f - (float) Math.pow(r_out_parallel.length(), 2)));
+        return r_out_parallel.plus(r_out_perp);
     }
 
     public static float dot(Vec3 vector1, Vec3 vector2) {
